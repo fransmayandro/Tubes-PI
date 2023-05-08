048f371b20171d461bf5a3e240bac9d0b7f603e2
fransmayandro <fransmbm@gmail.com>
2023-05-08 16:19:30 +0700

    Add Models, Routes

---
+++ app/Models/Jobsaved.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Jobsaved extends Model
+{
+    protected $table = 'saved_jobs';
+}
