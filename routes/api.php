20e66ec91c10d867628b15898916b6a415005a96
fransmayandro <fransmbm@gmail.com>
2023-05-09 12:37:13 +0700

    Job, Jobsaved, Company Post API

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Company;
-use App\Models\Job;
-use App\Models\Jobsaved;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\JobsavedController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,14 +21,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/company', function() {
-    return Company::all();
-});
+Route::get('/company', [CompanyController::class, 'index']);
 
-Route::get('/job', function() {
-    return Job::all();
-});
+Route::get('/job', [JobController::class, 'index']);
 
-Route::get('/jobsaved', function() {
-    return Jobsaved::all();
-});
+Route::get('/jobsaved', [JobsavedController::class, 'index']);
