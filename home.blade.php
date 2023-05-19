5d2890e5b480bc50c6c8cac0c470f05aba31c5f4
khairunisaftr <93196371+khairunisaftr@users.noreply.github.com>
2023-05-19 21:51:06 +0700

    Add files via upload

--- home.blade.php
+++ home.blade.php
@@ -1,5 +1,28 @@
-@extends('master')
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  </head>
+  <body>
 
-@section('konten')
-  <h4>Selamat Datang <b>{{Auth::user()->name}}</b>, Anda Login sebagai <b>{{Auth::user()->role}}</b>.</h4>
-@endsection
+  <div class="card">
+    <div class="card-body">
+        <div class="row justify-content-between">
+            <div class="col-md-9">
+             <h1> Selamat Datang di halaman {{ Auth::user()->role }}</h1>
+             <h1>{{ Auth::user()->name }} </h1>
+            </div>
+            <div class="col-md-1">
+                <a class="btn btn-primary" href="{{Auth::logout()}}">Logout</a>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  </body>
+</html>
+
--- login.blade.php
+++ login.blade.php
@@ -4,34 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login - Aplikasi Mencari Kerja</title>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <title>Login</title>
 </head>
 <body>
-    <div class="container"><br>
-        <div class="col-md-4 col-md-offset-4">
-            <h2 class="text-center"><b>INDEED</b><br>Aplikasi Mencari Kerja</h3>
-            <hr>
-            @if(session('error'))
+    <div class="container py-5">
+        <div class="w-50 center border rounded px-3 py-3 mx-auto">
+        <h1>Login</h1>
+        @if($errors->any())
             <div class="alert alert-danger">
-                <b>Opps!</b> {{session('error')}}
+                <ul>
+                    @foreach($errors->all() as $item)
+                    <li>{{$item}}</li>
+                    @endforeach
+                </ul>
             </div>
-            @endif
-            <form action="{{ route('actionlogin') }}" method="post">
+        @endif
+        <form action="" method="POST">
             @csrf
-                <div class="form-group">
-                    <label>Email</label>
-                    <input type="email" name="email" class="form-control" placeholder="Email" required="">
-                </div>
-                <div class="form-group">
-                    <label>Password</label>
-                    <input type="password" name="password" class="form-control" placeholder="Password" required="">
-                </div>
-                <button type="submit" class="btn btn-primary btn-block">Log In</button>
-                <hr>
-                <p class="text-center">Belum punya akun? <a href="#">Register</a> sekarang!</p>
-            </form>
-        </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" value="{{old('email')}}" name="email" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" name="password" class="form-control">
+            </div>
+            <div class="mb-3 d-grid">
+                <button name="submit" type="submit" class="btn btn-primary">Login</button>
+            </div>
+        </form>
+    </div> 
     </div>
 </body>
 </html>
